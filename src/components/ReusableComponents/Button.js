5ef578e439ea85f33df3568c1f1cf9a71488a3c9
lano0001 <lano0001@stud.kea.dk>
2024-12-04 15:41:04 +0000

    added button component

---
+++ src/components/ReusableComponents/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Button() {
+  return (
+    <div className="App">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      Lhelelellele
+    </div>
+  );
+}
+
+export default Button;
