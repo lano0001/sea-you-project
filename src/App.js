4629f2983240dfcbac8346f53905a11d96822eaa
Pemo0001 <pemo0001@stud.kea.dk>
2024-12-13 18:22:18 +0000

    Wave

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Button from "../src/components/ReusableComponents/Button";
+import Button from "./components/ReusableComponents/Button";
+import Wave1 from "./components/Wave1";
 
 function App() {
   return (
     <div className="">
       <Button />
+      <Wave1 />
     </div>
   );
 }
---
+++ src/components/Wave1.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Wave1.css";
+
+function Wave1() {
+  return (
+    <div className="waves">
+      <div class="wave" id="wave1"></div>
+      <div class="wave" id="wave2"></div>
+      <div class="wave" id="wave3"></div>
+      <div class="wave" id="wave4"></div>
+    </div>
+  );
+}
+
+export default Wave1;
