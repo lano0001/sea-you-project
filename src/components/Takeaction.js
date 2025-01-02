790128042ff7512f26997ed6288d7bea6665e2df
Malthe <maaa0001@stud.kea.dk>
2025-01-02 16:56:39 +0100

    TakeAction_2.0

--- src/components/Takeaction.js
+++ src/components/Takeaction.js
@@ -7,22 +7,31 @@ const TakeAction = () => {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        backgroundColor: "#e8f5fe",
-      }}
-    >
-      <ActionCard
-        image="https://example.com/image-url.jpg"
-        title="Norway: Stop deep sea mining!"
-        description="Norway is preparing to begin deep-sea mining in the Arctic, threatening one of the planet's most remote, ancient, and pristine ecosystems for the sake of profit."
-        buttonText="Take Action"
-        onAction={handleAction}
-      />
+    <div className="TakeAction">
+      <h2>Take Action</h2>
+      <div className="ActionCards">
+        <ActionCard
+          image="ice.webp"
+          title="Norway: Stop deep sea mining!"
+          description="Norway is preparing to begin deep-sea mining in the Arctic, threatening one of the planet's most remote, ancient, and pristine ecosystems for the sake of profit."
+          buttonText="Take Action"
+          onAction={handleAction}
+        />
+        <ActionCard
+          image="boats.webp"
+          title="Stop slavery at sea!"
+          description="Overfishing devastates marine life, ecosystems, and the livelihoods of millions."
+          buttonText="Take Action"
+          onAction={handleAction}
+        />
+        <ActionCard
+          image="seaMining.webp"
+          title="Stop deep sea mining!"
+          description="The deep sea, rich in biodiversity and vital in fighting climate change, faces a new threat: deep sea mining."
+          buttonText="Take Action"
+          onAction={handleAction}
+        />
+      </div>
     </div>
   );
 };
