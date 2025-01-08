5d7e7262b17b3dbd59b8d4ab5f4ab2bfff511309
Pemo0001 <pemo0001@stud.kea.dk>
2025-01-08 11:31:30 +0000

    fixes

--- src/components/HeyYou.js
+++ src/components/HeyYou.js
@@ -6,7 +6,7 @@ function HeyYou() {
     <div class="heyyou">
       <img class="exit" src="exit.png"></img>
       <div class="heyyoutext">
-        <h3>HEY YOU!</h3>
+        <h3 class="heyyou-h">HEY YOU!</h3>
         <p>You are right now in DarkMode/Low Power Mode, because its a more sustainable way to show our website. This means that you browsing this site uses less power, and therefore emits less CO2. We did this by making all the colors darker and monochrome and turning down the quality of the photos. If you want the full experience you have to turn the Low Power mode off, to get a more immersive experience.It was important for us to give you the choice!</p>
       </div>
     </div>
