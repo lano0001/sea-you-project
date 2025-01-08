0a94b2cbd7e680b4a37d4a1eb4ef80b2c758c08c
Malthe <maaa0001@stud.kea.dk>
2025-01-08 11:26:25 +0000

    Eventsite_Button

--- src/pages/EventSite.js
+++ src/pages/EventSite.js
@@ -1,9 +1,36 @@
+import Footer from "../components/Footer";
+import Button from "../components/ReusableComponents/Button";
 import "./EventSite.css";
 
 import { Link } from "react-router-dom";
 
 function EventSite() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <div className="hero"></div>
+      <h1>EVENT // TRASH PICKUP AT THE BEACH</h1>
+      <section className="content">
+        <p className="txt">
+          This month SEA YOU is organizing a trash pickup event at the Carcavelos Beach in Lisboa.
+          We encourage you to sign up and secure your spot! <br />
+          <br />
+          We will gather near the train station and walk to the beach together. We have arranged a
+          reservation at one of the beach cafés after the trash pickup where you will have a chance
+          to connect with other people in the SEA YOU family.
+        </p>
+        <h2>Location: Carcavelos Beach, Lisboa</h2>
+        <h2>Time: 16:00 </h2>
+        <div className="SignUpEvent">
+          <h2>Sign up for this event</h2>
+          <p>Want to secure YOUR spot for this event? Sign up with YOUR e-mail. </p>
+          <input type="email" placeholder="Your email" />
+          <Button text="SIGN UP" />
+        </div>
+      </section>
+      <img className="fb" src="fb.svg" alt="Facebook logo"></img>
+      <Footer bgColor={"blue2"} />
+    </div>
+  );
 }
 
 export default EventSite;
