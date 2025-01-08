61743605597003692408b241960c05c15f036cc3
Pemo0001 <pemo0001@stud.kea.dk>
2025-01-08 11:46:39 +0000

    sites

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Whoisseayou from "./components/Whoisseayou";
 import Whyyoushouldcare from "./components/Whyyoushouldcare";
-import TakeAction from "./components/TakeAction";
 import Whatyoucando from "./components/Whatyoucando";
 import Events from "./components/Events";
 import LandingPage from "./pages/LandingPage";
 
 import TakeActionSite1 from "./pages/TakeActionSite1";
+import TakeActionSite2 from "./pages/TakeActionSite2";
+import TakeActionSite3 from "./pages/TakeActionSite3";
 import Navigation from "./components/Navigation/Navigation";
 import EventSite from "./pages/EventSite";
 
@@ -28,6 +29,8 @@ function App() {
             <Route index element={<LandingPage />} />
 
             <Route path="/takeaction" element={<TakeActionSite1 />} />
+            <Route path="/takeaction1" element={<TakeActionSite2 />} />
+            <Route path="/takeaction2" element={<TakeActionSite3 />} />
             <Route path="/event" element={<EventSite />} />
           </Route>
         </Routes>
